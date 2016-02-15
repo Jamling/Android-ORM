33866af8e2c0fe79a41e5f601e184be6cebb63f8
Jamling <li.jamling@gmail.com>
2016-02-15 14:05:41 +0800

    add notifyChange method in mock ContentResolver

--- cn.ieclipse.aorm.mock/src/android/content/ContentResolver.java
+++ cn.ieclipse.aorm.mock/src/android/content/ContentResolver.java
@@ -64,4 +64,39 @@ public abstract class ContentResolver {
     public final void unregisterContentObserver(ContentObserver observer) {
     }
     
+    /**
+     * Notify registered observers that a row was updated and attempt to sync changes
+     * to the network.
+     * To register, call {@link #registerContentObserver(android.net.Uri , boolean, android.database.ContentObserver) registerContentObserver()}.
+     * By default, CursorAdapter objects will get this notification.
+     *
+     * @param uri The uri of the content that was changed.
+     * @param observer The observer that originated the change, may be <code>null</null>.
+     * The observer that originated the change will only receive the notification if it
+     * has requested to receive self-change notifications by implementing
+     * {@link ContentObserver#deliverSelfNotifications()} to return true.
+     */
+    public void notifyChange(Uri uri, ContentObserver observer) {
+        notifyChange(uri, observer, true /* sync to network */);
+    }
+
+    /**
+     * Notify registered observers that a row was updated.
+     * To register, call {@link #registerContentObserver(android.net.Uri , boolean, android.database.ContentObserver) registerContentObserver()}.
+     * By default, CursorAdapter objects will get this notification.
+     * If syncToNetwork is true, this will attempt to schedule a local sync using the sync
+     * adapter that's registered for the authority of the provided uri. No account will be
+     * passed to the sync adapter, so all matching accounts will be synchronized.
+     *
+     * @param uri The uri of the content that was changed.
+     * @param observer The observer that originated the change, may be <code>null</null>.
+     * The observer that originated the change will only receive the notification if it
+     * has requested to receive self-change notifications by implementing
+     * {@link ContentObserver#deliverSelfNotifications()} to return true.
+     * @param syncToNetwork If true, attempt to sync the change to the network.
+     * @see #requestSync(android.accounts.Account, String, android.os.Bundle)
+     */
+    public void notifyChange(Uri uri, ContentObserver observer, boolean syncToNetwork) {
+        // notifyChange(uri, observer, syncToNetwork, UserHandle.myUserId());
+    }
 }
